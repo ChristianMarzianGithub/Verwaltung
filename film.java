0231640b99699027c777caf4ae715a63add9a181
randwerArbeit <marzian@innosoft.de>
2016-12-19 12:32:18 +0100

    initial commit

---
+++ film.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.util.Date;
+
+public class Film {
+
+	private String title;
+	private String storyLine;
+	private Date releaseDate = new Date();
+	private int runTimeMin;
+	private String country;
+	
+	
+	public Film(){
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStoryLine() {
+		return storyLine;
+	}
+
+	public void setStoryLine(String storyLine) {
+		this.storyLine = storyLine;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public int getRunTimeMin() {
+		return runTimeMin;
+	}
+
+	public void setRunTimeMin(int runTimeMin) {
+		this.runTimeMin = runTimeMin;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
+}
